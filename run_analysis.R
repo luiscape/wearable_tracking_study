c11eb24a965ed6d13c03aebcb20cbd06b75e9fce
Luis Capelo <luiscape@gmail.com>
2014-05-25 18:06:41 -0400

    finished the R code

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,9 @@
 # You should create one R script called run_analysis.R that does the following. 
 
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive activity names. 
-# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+library(reshape2)  # for using melt. 
+library(plyr)  # for using rbind.fill
 
-
+## FETCHING DATA ##
 # Opening a temporary connection and downloading the data.
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
@@ -32,17 +31,18 @@ featuresList <- as.list(featuresList[2])
 featuresList <- paste(featuresList[[1]])
 
 
-# 1. Merges the training and the test sets to create one data set.
-# Adding the original variable names. 
+## STEP 1 ##
+# Merging the training and the test sets to create one data set.
+
+# Adding the original variable names to the Train dataset.
 names(xTrain) <- paste0(featuresList)
 colnames(yTrain)[1] <- 'subject'
 trainCombined <- cbind(yTrain, xTrain)
 
+# Adding the original variable names to the Test dataset.
 names(xTest) <- paste0(featuresList)
 colnames(yTest)[1] <- 'subject'
 testCombined <- cbind(yTest, xTest)
-
-
 
 # Adding labels for test and train datasets. 
 trainCombined$label <- 'Train'
@@ -51,7 +51,11 @@ testCombined$label <- 'Test'
 # Merging both test and train datasets. 
 mergedDataset <- rbind(trainCombined, testCombined)
 
-# 3.Uses descriptive activity names to name the activities in the data set
+
+## STEP 2 ##
+# Using descriptive activity names to name the activities in the data set
+
+# For loop that adds labels to the activities. 
 for (i in 1:nrow(activityLabels)) {
     testCombined$subject <- gsub(i, as.character(activityLabels[i, 2]), testCombined$subject)
     trainCombined$subject <- gsub(i, as.character(activityLabels[i, 2]), trainCombined$subject)
@@ -62,7 +66,9 @@ for (i in 1:nrow(activityLabels)) {
 }
 
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## STEP 3 ##
+# Extracting only the measurements on the mean and standard deviation for each measurement.
+
 # This regex allows me to identify what are the variables that have
 # the mean or the standard deviation in them. 
 meanIndex <- which(grepl(pattern = "-mean()", names(mergedDataset)))
@@ -77,66 +83,156 @@ meanstdData <- data.frame(mergedDataset[, meanstdIndex])
 
 
 
-# 4. Appropriately labels the data set with descriptive activity names. 
+## STEP 4 ##
+# Appropriately labeling the data set with descriptive activity names. 
 
-#### Naming function ####
-#
-# The structure of names is the following: 
-#
-#          1 2   3    4     5
-#          ↓ ↓   ↓    ↓     ↓
-#          tBodyGyro.mean...X
-# 
-# 1. A letter (t or f): 
-# 2. A type of capture (Body, BodyBody or Gravity): 
-# 3. The sensor (Acc, AccJerk, AccMag, AccJerkMag, Gyro, GyroJerk, GyroMag): 
-# 4. The measurement (mean, std, meanFreq):
-# 5. A trailing letter (X, Y or Z) for the 3-axial signals.
+# Here I am using the Tidy Data article by Hadley Wickham to determine 
+# how to create correct variable columns.
 
-
-
-
-ProperNaming <- function(df = NULL) { 
+TidyThisData <- function(df = NULL) { 
     
-    newNames <- names(meanstdData)
- 
-    # Renaming the labels and activity.
-    newNames <- sub("activity", "Activity", newNames)  # Time signal.
-    newNames <- sub("label", "Label", newNames)  # Time signal.
+    message('Using regex to extract the appropriate variables.')
     
-    # Renaming the signal letters.
-    newNames[2:40] <- sub("t", "TimeSignal", newNames[2:40])  # Time signal.
-    newNames[40:79] <- sub("f", "FFTransformation", newNames[40:79])  # FFT transformation.
+    # Index for the activity and label.
+    tidyActivityIndex <- which(grepl(pattern = "activity", names(df), fixed = T))
+    tidyLabelIndex <- which(grepl(pattern = "label", names(df), fixed = T))
     
-    # Renaming the sensor type.
-    newNames <- gsub("Acc", "Accelerometer", newNames)
-    newNames <- gsub("AccelerometerJerkMag", "AccelerometerJerkMagnitude", newNames)
-    newNames <- gsub("AccelerometerMag", "Accelerometer", newNames)    
-    newNames <- gsub("Gyro", "Gyroscope", newNames)
-    newNames <- gsub("GyroscopeMag", "GyroscopeMagnitude", newNames)
+    ## Signals
+    # Time signal
+    tidyIndex1 <- which(grepl(pattern = "t", names(meanstdData[1:41]), fixed = TRUE))
+    tidyTimeIndex <- c(tidyLabelIndex, tidyIndex1)
+    SignalTimeData <- data.frame(meanstdData[, tidyTimeIndex])
+    SignalTimeData$Signal <- 'Time'
+    
+    # Fast Fourier transform signal
+    tidyIndex2 <- which(grepl(pattern = "f", names(meanstdData), fixed = TRUE))
+    tidyFFTIndex <- c(tidyActivityIndex, tidyLabelIndex, tidyIndex2)
+    SignalFFTData <- data.frame(meanstdData[, tidyFFTIndex])
+    SignalFFTData$Signal <- 'Fast Fourier Transform'
+    
+    signalDFOutput <<- rbind.fill(SignalTimeData, SignalFFTData)
+    
+    regexList <- c('BodyBody', 'Body', 'Gravity', 
+                   'AccJerkMag', 'AccMag', 'AccJerk', 'Acc',
+                   'GyroMag', 'GyroJerk', 'Gyro', 
+                   '.mean', '.std', '.meanFreq', 
+                   '...X', '...Y', '...Y')
+    
+    variableList <- c('BodyBody', 'Body', 'Gravity', 
+                      'Accelerometer Jerk Magnitude', 'Accelerometer Magnitude', 
+                      'Accelerometer Jerk', 'Accelerometer', 
+                      'Gyroscope Magnitude', 'Gyroscope Jerk', 'Gyroscope',
+                      'Mean', 'Standard Deviation', 'Mean Frequency', 
+                      'X', 'Y', 'Z')
+    
+    
+    for (i in 1:length(regexList)) {
         
-    # Renaming the measurement.
-    newNames <- gsub(".mean", "_Mean", newNames)
-    newNames <- gsub(".std", "_StandardDeviation", newNames)
-    newNames <- gsub(".meanFreq", "_MeanFrequency", newNames)
+        if (i > 0 && i <= 3) {  # for the capture labelling
+            
+            tidyIndex <- which(grepl(pattern = regexList[i], 
+                                     names(df), fixed = TRUE))
+            
+            tidyCaptureIndex <- c(tidyActivityIndex, tidyLabelIndex, tidyIndex)
+            captureDataframe <- data.frame(df[, tidyCaptureIndex])
+            captureDataframe$Capture <- variableList[i]
+            if (i == 1) { captureDFOutput <- captureDataframe }
+            else { captureDFOutput <<- rbind.fill(captureDFOutput, captureDataframe) } 
+            
+        }
+        
+        if (i > 3 && i <= 11) {  # for the sensor labelling
+            
+            tidyIndex <- which(grepl(pattern = regexList[i], 
+                                     names(df), fixed = TRUE))
+            
+            tidySensorIndex <- c(tidyActivityIndex, tidyLabelIndex, tidyIndex)
+            sensorDataframe <- data.frame(df[, tidySensorIndex])
+            sensorDataframe$Sensor <- variableList[i]
+            if (i == 4) { sensorDFOutput <- sensorDataframe }
+            else { sensorDFOutput <- rbind.fill(sensorDFOutput, sensorDataframe) } 
+            
+        }
+        
+        if (i > 11 &&  i <= 13) {  # for the measurement 
+            
+            tidyIndex <- which(grepl(pattern = regexList[i], 
+                                     names(df), fixed = TRUE))
+            
+            tidyMeasurementIndex <- c(tidyActivityIndex, tidyLabelIndex, tidyIndex)
+            measurementDataframe <- data.frame(df[, tidyMeasurementIndex])
+            measurementDataframe$Measurement <- variableList[i]
+            if (i == 12) { measurementDFOutput <- measurementDataframe }
+            else { measurementDFOutput <- rbind.fill(measurementDFOutput, measurementDataframe) } 
+        }
+        
+        if (i > 13 &&  i <= 16) {  # for the axis 
+            
+            tidyIndex <- which(grepl(pattern = regexList[i], 
+                                     names(df), fixed = TRUE))
+            
+            names(df) <- gsub(pattern = regexList[i], "xx", names(df), fixed = TRUE)
+            
+            tidyAxisIndex <- c(tidyActivityIndex, tidyLabelIndex, tidyIndex)
+            axisDataframe <- data.frame(df[, tidyAxisIndex])
+            axisDataframe$Axis <- variableList[i]
+            if (i == 14) { axisDFOutput <- axisDataframe }
+            else { axisDFOutput <- rbind.fill(axisDFOutput, axisDataframe) } 
+        }
     
-    # Renaming the final letters.
-    newNames <- gsub("...X", "XAxis", newNames)
-    newNames <- gsub("...Y", "YAxis", newNames)
-    newNames <- gsub("...Z", "ZAxis", newNames)
-    
-    # Cleaning up.
-    newNames <- gsub("..", "", newNames, fixed = TRUE)
+    }
 
-    names(df) <- newNames
+    message('Using `plyr` to melt the variable data.frames.')
     
-    df
+    signalDFOutputMelt <- melt(signalDFOutput)
+    captureDFOutputMelt <- melt(captureDFOutput)
+    sensorDFOutputMelt <- melt(sensorDFOutput)
+    measurementDFOutputMelt <- melt(measurementDFOutput)
+    axisDFOutputMelt <- melt(axisDFOutput)
+    
+    message('Merging the variable data.frames into a single data.frame.')
+    
+    tidyData <- merge(signalDFOutputMelt, captureDFOutputMelt, 
+                      by = c('label', 'activity', 'value'))
+    
+    tidyData$variable <- NULL
+    tidyData$variable <- NULL
+    
+    tidyData <- merge(tidyData, sensorDFOutputMelt, 
+                      by = c('label', 'activity', 'value'))
+    
+    tidyData$variable <- NULL
+    
+    tidyData <- merge(tidyData, measurementDFOutputMelt, 
+                      by = c('label', 'activity', 'value'))
+    
+    tidyData$variable <- NULL
+    
+    tidyData <- merge(tidyData, axisDFOutputMelt, 
+                      by = c('label', 'activity', 'value'))
+    
+    tidyData$variable <- NULL
+    tidyData$variable.x <- NULL
+    tidyData$variable.y <- NULL
+    
+    print(names(tidyData))
+    
+    message('Cleaning ...')
+    
+    # Cleaning labels.
+    tidyLabels <- c('Label', 'Activity', 'Value', 'Signal', 
+                    'Capture', 'Sensor', 'Measurement', 'Axis')
+    
+    names(tidyData) <- tidyLabels
+          
+    message('Done!')
+    
+    tidyData
 }
 
-meanstdData <- ProperNaming(meanstdData)
+TidyData <- TidyThisData(meanstdData)
 
 
 # Storing the resulting dataset.
-write.csv(meanstdData, 'data/TidyData.csv', row.names = FALSE)
-
+write.csv(TidyData, 'data/TidyData.csv', row.names = FALSE)
 
